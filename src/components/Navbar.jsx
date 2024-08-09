02e3ce9c1f9ced47f1b8148d31908e2f2ec5a35d
Satyadev <126260629+ns0851@users.noreply.github.com>
2024-08-09 10:14:42 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+iniimport React from 'react'
 import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
 
@@ -21,7 +21,7 @@ const Navbar = () => {
     <div className='flex justify-between bg-[#181818] text-white items-center p-6 font-light text-xl '>
       <div className='nav flex gap-4 items-center'>
         <img className='h-16' src="../../bull.svg" alt="" />
-        <div className='text-6xl font-head'>Lamborghini</div>
+        <div className='text-6xl font-head'>Fakerghini</div>
       </div>
       <div className='flex gap-7 ul'>
         <p>Home</p>
